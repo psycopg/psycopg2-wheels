83a19200675b4d09d6718884517227414cb52f8b
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-02-01 14:21:46 +0000

    Trying parallel build for 32/64 bits wheels

--- scripts/travis-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-docker run -t --rm -v `pwd`:/build quay.io/pypa/manylinux1_x86_64 /build/scripts/build-manylinux.sh
-
-docker run -t --rm -v `pwd`:/build quay.io/pypa/manylinux1_i686 linux32 /build/scripts/build-manylinux.sh
-
