366d2007e4ce0dee264b18a0e073b08b47f95680
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-02-05 15:35:31 +0100

    chdir into whatever directory extracted from downloaded package

--- scripts/build_dinosaur.sh
+++ scripts/build_dinosaur.sh
@@ -9,9 +9,12 @@ set -e -x
 export PACKAGE=${PACKAGE:-$(echo "$PGVER" | sed 's/\([0-9]\+\.[0-9]\+\).*/\1/')}
 export URL=${URL:-https://ftp.postgresql.org/pub/source/v${PGVER}/postgresql-${PGVER}.tar.gz}
 
+# Download into a directory and try to work out what download
+mkdir incoming
+cd incoming
 wget -O - "$URL" | tar xzf -
+cd $(ls -t1 | head -1)
 
-cd "postgresql-$PGVER"
 ./configure --prefix "/opt/postgresql-${PACKAGE}"
 make
 sudo make install
