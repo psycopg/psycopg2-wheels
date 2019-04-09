193785a056b93475ef9986b9aadc7e1168521f0f
Matthew Brett <matthew.brett@gmail.com>
2019-04-09 10:25:20 +0100

    Turn of tests to make install faster

--- scripts/build_libpq.sh
+++ scripts/build_libpq.sh
@@ -21,8 +21,7 @@ if [ ! -d "openssl-${OPENSSL_TAG}/" ]; then
     # Need perl 5.10.0 for build
     curl -L https://install.perlbrew.pl | bash
     source ~/perl5/perlbrew/etc/bashrc
-    perlbrew install perl-5.16.0
-    perlbrew switch perl-5.16.0
+    perlbrew install --notest --switch perl-5.16.0
 
     curl -sL \
         https://github.com/openssl/openssl/archive/${OPENSSL_TAG}.tar.gz \
