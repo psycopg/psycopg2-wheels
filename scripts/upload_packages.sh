a980e9efb5ca0f58491f6d60e9e0635003af9e95
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2019-04-11 02:44:00 +0100

    Skip upload if the encryption key is missing
    Useful for pull requests from other users who don't have the key.

--- scripts/upload_packages.sh
+++ scripts/upload_packages.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-set -e -x
+set -e
+
+if [ "$encrypted_bd656d6a5753_key" == "" ]; then
+    echo "encrypted key missing: skipping upload" >&2
+    exit 0
+fi
+
+# Set up files required for upload
+openssl aes-256-cbc \
+    -K $encrypted_bd656d6a5753_key -iv $encrypted_bd656d6a5753_iv \
+    -in id_rsa-travis-upload.enc -out /tmp/id_rsa-travis-upload -d
+
+chmod 600 known_hosts /tmp/id_rsa-travis-upload
 
 # Print sha1 checksum in portable format. You can copy the output of this
 # command and paste it into `shasum -c` to verify packages downloaded locally.
