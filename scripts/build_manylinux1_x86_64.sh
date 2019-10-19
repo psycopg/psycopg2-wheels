66b921d564cf88ac39b906d66906a5948e348fd3
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2019-10-19 19:59:43 +0100

    Friendly and less so reminders to set certain env vars

--- scripts/build_manylinux1_x86_64.sh
+++ scripts/build_manylinux1_x86_64.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -euo pipefail
+# set -x
+
 docker run --rm -v $TRAVIS_BUILD_DIR:/build \
     -e PSYCOPG2_TESTDB_USER=postgres -e PSYCOPG2_TEST_FAST=1 \
     -e WANT_LIBPQ -e PACKAGE_NAME \
