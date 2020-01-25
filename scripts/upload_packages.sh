cb62757761214d2bfa27eb6ff1abea60d0c00a6c
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-01-25 21:37:08 +0000

    Deploy packages using the deploy step
    The pipeline doesn't fail if the after_success step fails.
    
    Also better safe mode and some bash idiomatisms in the upload script.

--- scripts/upload_packages.sh
+++ scripts/upload_packages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+# set -x
 
-if [ "$encrypted_bd656d6a5753_key" == "" ]; then
+if [ -z "${encrypted_bd656d6a5753_key:-}" ]; then
     echo "encrypted key missing: skipping upload" >&2
     exit 0
 fi
@@ -12,7 +13,7 @@ openssl aes-256-cbc \
     -K $encrypted_bd656d6a5753_key -iv $encrypted_bd656d6a5753_iv \
     -in data/id_rsa-psycopg-upload.enc -out data/id_rsa-psycopg-upload -d
 
-chmod 600 data/known_hosts data/id_rsa-psycopg-upload data/ssh_config
+chmod 600 data/{known_hosts,id_rsa-psycopg-upload,ssh_config}
 
 # Print sha1 checksum in portable format. You can copy the output of this
 # command and paste it into `shasum -c` to verify packages downloaded locally.
